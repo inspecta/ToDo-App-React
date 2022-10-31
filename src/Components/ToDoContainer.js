b26a6cbf8512469e7a1ddd578ab2db4ec12e957a
Derrick <djmulinde@gmail.com>
2022-10-31 17:16:35 +0300

    Fetch data from api and save to localstorage

--- src/Components/ToDoContainer.js
+++ src/Components/ToDoContainer.js
@@ -6,23 +6,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 class ToDoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false
-      }
-    ]
+    todos: [],
   };
 
   handleChange = (id) => {
@@ -69,6 +53,27 @@ class ToDoContainer extends React.Component {
         return todo;
       })
     })
+  }
+
+  // componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ todos: data }));
+  // }
+  componentDidMount() {
+    const temp = localStorage.getItem("todos");
+    if (JSON.parse(temp)) {
+      this.setState({
+        todos: JSON.parse(temp),
+      })
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos);
+      localStorage.setItem("todos", temp);
+    }
   }
 
   render() {
